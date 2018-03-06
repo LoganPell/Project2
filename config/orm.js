5f2aaea1b67117a4ecbe234d4f780d95f246fc77
LoganPell <logan.pell@gmail.com>
2018-03-06 11:32:46 -0600

    rendering home page again

--- config/orm.js
+++ config/orm.js
@@ -1,139 +1,139 @@
-// Connection to MySql
-var connection = require('../db.js');
+// // Connection to MySql
+// var connection = require('../db.js');
 
-// Object for all our SQL statement functions.
-// Setup CRUD for MySQL
+// // Object for all our SQL statement functions.
+// // Setup CRUD for MySQL
 
-var ormPost = {
-  // View all posts
-  all: function(tableInput, cb) {
-    var queryString = "SELECT * FROM posts" + tableInput + ";";
-    connection.query(queryString, (err, result) => {
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
-  },
-  create: function(table, cols, vals, cb) {
-    var queryString = "INSERT INTO " + table;
+// var ormPost = {
+//   // View all posts
+//   all: function(tableInput, cb) {
+//     var queryString = "SELECT * FROM posts" + tableInput + ";";
+//     connection.query(queryString, (err, result) => {
+//       if (err) {
+//         throw err;
+//       }
+//       cb(result);
+//     });
+//   },
+//   create: function(table, cols, vals, cb) {
+//     var queryString = "INSERT INTO " + table;
 
-    queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
+//     queryString += " (";
+//     queryString += cols.toString();
+//     queryString += ") ";
+//     queryString += "VALUES (";
+//     queryString += printQuestionMarks(vals.length);
+//     queryString += ") ";
 
-    console.log(queryString);
+//     console.log(queryString);
 
-    connection.query(queryString, vals, (err, result) => {
-      if (err) {
-        throw err;
-      }
+//     connection.query(queryString, vals, (err, result) => {
+//       if (err) {
+//         throw err;
+//       }
 
-      cb(result);
-    });
-  },
-  // An example of objColVals would be {name: panther, sleepy: true}
-  update: function(table, objColVals, post, cb) {
-    var queryString = "UPDATE " + table;
+//       cb(result);
+//     });
+//   },
+//   // An example of objColVals would be {name: panther, sleepy: true}
+//   update: function(table, objColVals, post, cb) {
+//     var queryString = "UPDATE " + table;
 
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += post;
+//     queryString += " SET ";
+//     queryString += objToSql(objColVals);
+//     queryString += " WHERE ";
+//     queryString += post;
 
-    console.log(queryString);
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
+//     console.log(queryString);
+//     connection.query(queryString, function(err, result) {
+//       if (err) {
+//         throw err;
+//       }
 
-      cb(result);
-    });
-  },
+//       cb(result);
+//     });
+//   },
 
-  delete: function(table, post, cb) {
-    var queryString = "DELETE FROM " + table;
-    queryString += " WHERE ";
-    queryString += post;
+//   delete: function(table, post, cb) {
+//     var queryString = "DELETE FROM " + table;
+//     queryString += " WHERE ";
+//     queryString += post;
 
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
+//     connection.query(queryString, function(err, result) {
+//       if (err) {
+//         throw err;
+//       }
 
-      cb(result);
-    });
-  }
-};
+//       cb(result);
+//     });
+//   }
+// };
 
-var ormBlogger = {
-  all: function(tableInput, cb) {
-    var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, (err, result) => {
-      if (err) {
-        throw err;
-      }
-      cb(result);
-    });
-  },
-  create: function(table, cols, vals, cb) {
-    var queryString = "INSERT INTO " + table;
+// var ormBlogger = {
+//   all: function(tableInput, cb) {
+//     var queryString = "SELECT * FROM " + tableInput + ";";
+//     connection.query(queryString, (err, result) => {
+//       if (err) {
+//         throw err;
+//       }
+//       cb(result);
+//     });
+//   },
+//   create: function(table, cols, vals, cb) {
+//     var queryString = "INSERT INTO " + table;
 
-    queryString += " (";
-    queryString += cols.toString();
-    queryString += ") ";
-    queryString += "VALUES (";
-    queryString += printQuestionMarks(vals.length);
-    queryString += ") ";
+//     queryString += " (";
+//     queryString += cols.toString();
+//     queryString += ") ";
+//     queryString += "VALUES (";
+//     queryString += printQuestionMarks(vals.length);
+//     queryString += ") ";
 
-    console.log(queryString);
+//     console.log(queryString);
 
-    connection.query(queryString, vals, (err, result) => {
-      if (err) {
-        throw err;
-      }
+//     connection.query(queryString, vals, (err, result) => {
+//       if (err) {
+//         throw err;
+//       }
 
-      cb(result);
-    });
-  },
-  // An example of objColVals would be {name: panther, sleepy: true}
-  update: function(table, objColVals, blogger, cb) {
-    var queryString = "UPDATE " + table;
+//       cb(result);
+//     });
+//   },
+//   // An example of objColVals would be {name: panther, sleepy: true}
+//   update: function(table, objColVals, blogger, cb) {
+//     var queryString = "UPDATE " + table;
 
-    queryString += " SET ";
-    queryString += objToSql(objColVals);
-    queryString += " WHERE ";
-    queryString += blogger;
+//     queryString += " SET ";
+//     queryString += objToSql(objColVals);
+//     queryString += " WHERE ";
+//     queryString += blogger;
 
-    console.log(queryString);
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
+//     console.log(queryString);
+//     connection.query(queryString, function(err, result) {
+//       if (err) {
+//         throw err;
+//       }
 
-      cb(result);
-    });
-  },
+//       cb(result);
+//     });
+//   },
 
-  delete: function(table, blogger, cb) {
-    var queryString = "DELETE FROM " + table;
-    queryString += " WHERE ";
-    queryString += blogger;
+//   delete: function(table, blogger, cb) {
+//     var queryString = "DELETE FROM " + table;
+//     queryString += " WHERE ";
+//     queryString += blogger;
 
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
+//     connection.query(queryString, function(err, result) {
+//       if (err) {
+//         throw err;
+//       }
 
-      cb(result);
-    });
-  }
-};
+//       cb(result);
+//     });
+//   }
+// };
 
-// Export the orm object for the model (blogger.js / post.js).
-module.exports = ormPost;
-module.exports = ormBlogger;
+// // Export the orm object for the model (blogger.js / post.js).
+// module.exports = ormPost;
+// module.exports = ormBlogger;
 
